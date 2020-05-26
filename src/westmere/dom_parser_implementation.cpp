2dcb54c29090a9192e97a099bdc5c2342e154158
John Keiser <john@johnkeiser.com>
2020-05-26 16:57:27 -0700

    Store all parser state in the implementation

--- src/westmere/dom_parser_implementation.cpp
+++ src/westmere/dom_parser_implementation.cpp
@@ -73,8 +73,8 @@ WARN_UNUSED error_code implementation::minify(const uint8_t *buf, size_t len, ui
 
 #include "generic/stage1/utf8_lookup2_algorithm.h"
 #include "generic/stage1/json_structural_indexer.h"
-WARN_UNUSED error_code dom_parser_implementation::stage1(const uint8_t *buf, size_t len, parser &parser, bool streaming) noexcept {
-  return westmere::stage1::json_structural_indexer::index<64>(buf, len, parser, streaming);
+WARN_UNUSED error_code dom_parser_implementation::stage1(const uint8_t *_buf, size_t _len, bool streaming) noexcept {
+  return westmere::stage1::json_structural_indexer::index<64>(_buf, _len, *this, streaming);
 }
 
 } // namespace westmere
@@ -96,6 +96,12 @@ namespace westmere {
 #include "generic/stage2/structural_iterator.h"
 #include "generic/stage2/structural_parser.h"
 #include "generic/stage2/streaming_structural_parser.h"
+
+WARN_UNUSED error_code dom_parser_implementation::parse(const uint8_t *_buf, size_t _len, dom::document &_doc) noexcept {
+  error_code err = stage1(_buf, _len, false);
+  if (err) { return err; }
+  return stage2(_doc);
+}
 
 } // namespace westmere
 } // namespace simdjson
