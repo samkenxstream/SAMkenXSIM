White-Emperor-Sword/simdjson

On-demand parse implementation

src/generic/ondemand.h
@@ -1,0 +1,15 @@
+#include "generic/ondemand/raw_json_string.h"
+#include "generic/ondemand/value.h"
+#include "generic/ondemand/json.h"
+#include "generic/ondemand/array.h"
+#include "generic/ondemand/field.h"
+#include "generic/ondemand/object.h"
+#include "generic/ondemand/parser.h"
+
+#include "generic/ondemand/raw_json_string-inl.h"
+#include "generic/ondemand/json-inl.h"
+#include "generic/ondemand/value-inl.h"
+#include "generic/ondemand/array-inl.h"
+#include "generic/ondemand/field-inl.h"
+#include "generic/ondemand/object-inl.h"
+#include "generic/ondemand/parser-inl.h"

samkenxstream/SAMkenXSIM

src/generic/ondemand.h
@@ -1,0 +1,19 @@
+#include "generic/ondemand/logger.h"
+#include "generic/ondemand/raw_json_string.h"
+#include "generic/ondemand/token_iterator.h"
+#include "generic/ondemand/document.h"
+#include "generic/ondemand/value.h"
+#include "generic/ondemand/array.h"
+#include "generic/ondemand/field.h"
+#include "generic/ondemand/object.h"
+#include "generic/ondemand/parser.h"
+
+#include "generic/ondemand/logger-inl.h"
+#include "generic/ondemand/raw_json_string-inl.h"
+#include "generic/ondemand/token_iterator-inl.h"
+#include "generic/ondemand/document-inl.h"
+#include "generic/ondemand/value-inl.h"
+#include "generic/ondemand/array-inl.h"
+#include "generic/ondemand/field-inl.h"
+#include "generic/ondemand/object-inl.h"
+#include "generic/ondemand/parser-inl.h"