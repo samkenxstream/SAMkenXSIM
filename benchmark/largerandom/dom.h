ZhijiangXu/simdjson

Add a "sum" benchmark with no appending to vector

benchmark/largerandom/dom.h
@@ -23,8 +23,7 @@ private:
 simdjson_really_inline bool Dom::Run(const padded_string &json) {
   container.clear();
 
-  dom::element doc = parser.parse(json);
-  for (auto point : doc) {
+  for (auto point : parser.parse(json)) {
     container.emplace_back(my_point{point["x"], point["y"], point["z"]});
   }
 
@@ -32,6 +31,35 @@ simdjson_really_inline bool Dom::Run(const padded_string &json) {
 }
 
 BENCHMARK_TEMPLATE(LargeRandom, Dom);
+
+class DomSum {
+public:
+  simdjson_really_inline bool Run(const padded_string &json);
+
+  simdjson_really_inline my_point &Result() { return sum; }
+  simdjson_really_inline size_t ItemCount() { return count; }
+
+private:
+  dom::parser parser{};
+  my_point sum{};
+  size_t count{};
+};
+
+simdjson_really_inline bool DomSum::Run(const padded_string &json) {
+  sum = { 0, 0, 0 };
+  count = 0;
+
+  for (auto coord : parser.parse(json)) {
+    sum.x += double(coord["x"]);
+    sum.y += double(coord["y"]);
+    sum.z += double(coord["z"]);
+    count++;
+  }
+
+  return true;
+}
+
+BENCHMARK_TEMPLATE(LargeRandom, DomSum);
 
 } // namespace largerandom
 

benchmark/largerandom/iter.h
@@ -51,6 +51,41 @@ simdjson_really_inline bool Iter::Run(const padded_string &json) {
 
 BENCHMARK_TEMPLATE(LargeRandom, Iter);
 
+class IterSum {
+public:
+  simdjson_really_inline bool Run(const padded_string &json);
+
+  simdjson_really_inline my_point &Result() { return sum; }
+  simdjson_really_inline size_t ItemCount() { return count; }
+
+private:
+  ondemand::parser parser{};
+  my_point sum{};
+  size_t count{};
+};
+
+simdjson_really_inline bool IterSum::Run(const padded_string &json) {
+  sum = {0,0,0};
+  count = 0;
+
+  auto iter = parser.iterate_raw(json).value();
+  if (!iter.start_array()) { return false; }
+  do {
+    if (!iter.start_object()   || !iter.find_field_raw("x")) { return false; }
+    sum.x += iter.get_double();
+    if (!iter.has_next_field() || !iter.find_field_raw("y")) { return false; }
+    sum.y +=  iter.get_double();
+    if (!iter.has_next_field() || !iter.find_field_raw("z")) { return false; }
+    sum.z +=  iter.get_double();
+    if (iter.skip_container()) { return false; } // Skip the rest of the tweet object
+    count++;
+  } while (iter.has_next_element());
+
+  return true;
+}
+
+BENCHMARK_TEMPLATE(LargeRandom, IterSum);
+
 }
 } // namespace largerandom
 

benchmark/largerandom/ondemand.h
@@ -40,6 +40,38 @@ simdjson_really_inline bool OnDemand::Run(const padded_string &json) {
 
 BENCHMARK_TEMPLATE(LargeRandom, OnDemand);
 
+class OnDemandSum {
+public:
+  simdjson_really_inline bool Run(const padded_string &json);
+
+  simdjson_really_inline my_point &Result() { return sum; }
+  simdjson_really_inline size_t ItemCount() { return count; }
+
+private:
+  ondemand::parser parser{};
+  my_point sum{};
+  size_t count{};
+};
+
+simdjson_really_inline bool OnDemandSum::Run(const padded_string &json) {
+  sum = {0,0,0};
+  count = 0;
+
+  auto doc = parser.iterate(json);
+  // TODO this sucks, you should be able to just say for ( ... : doc)
+  auto array = doc.get_array();
+  for (ondemand::object coord : array) {
+    sum.x += double(coord["x"]);
+    sum.y += double(coord["y"]);
+    sum.z += double(coord["z"]);
+    count++;
+  }
+
+  return true;
+}
+
+BENCHMARK_TEMPLATE(LargeRandom, OnDemandSum);
+
 }
 } // namespace largerandom
 

samkenxstream/SAMkenXSIM

benchmark/largerandom/dom.h
@@ -23,8 +23,7 @@ private:
 simdjson_really_inline bool Dom::Run(const padded_string &json) {
   container.clear();
 
-  dom::element doc = parser.parse(json);
-  for (auto point : doc) {
+  for (auto point : parser.parse(json)) {
     container.emplace_back(my_point{point["x"], point["y"], point["z"]});
   }
 
@@ -33,6 +32,38 @@ simdjson_really_inline bool Dom::Run(const padded_string &json) {
 
 BENCHMARK_TEMPLATE(LargeRandom, Dom);
 
+namespace sum {
+
+class Dom {
+public:
+  simdjson_really_inline bool Run(const padded_string &json);
+
+  simdjson_really_inline my_point &Result() { return sum; }
+  simdjson_really_inline size_t ItemCount() { return count; }
+
+private:
+  dom::parser parser{};
+  my_point sum{};
+  size_t count{};
+};
+
+simdjson_really_inline bool Dom::Run(const padded_string &json) {
+  sum = { 0, 0, 0 };
+  count = 0;
+
+  for (auto coord : parser.parse(json)) {
+    sum.x += double(coord["x"]);
+    sum.y += double(coord["y"]);
+    sum.z += double(coord["z"]);
+    count++;
+  }
+
+  return true;
+}
+
+BENCHMARK_TEMPLATE(LargeRandomSum, Dom);
+
+} // namespace sum
 } // namespace largerandom
 
 #endif // SIMDJSON_EXCEPTIONS

benchmark/largerandom/iter.h
@@ -51,7 +51,48 @@ simdjson_really_inline bool Iter::Run(const padded_string &json) {
 
 BENCHMARK_TEMPLATE(LargeRandom, Iter);
 
+} // unnamed namespace
+
+namespace sum {
+namespace {
+
+class Iter {
+public:
+  simdjson_really_inline bool Run(const padded_string &json);
+
+  simdjson_really_inline my_point &Result() { return sum; }
+  simdjson_really_inline size_t ItemCount() { return count; }
+
+private:
+  ondemand::parser parser{};
+  my_point sum{};
+  size_t count{};
+};
+
+simdjson_really_inline bool Iter::Run(const padded_string &json) {
+  sum = {0,0,0};
+  count = 0;
+
+  auto iter = parser.iterate_raw(json).value();
+  if (!iter.start_array()) { return false; }
+  do {
+    if (!iter.start_object()   || !iter.find_field_raw("x")) { return false; }
+    sum.x += iter.get_double();
+    if (!iter.has_next_field() || !iter.find_field_raw("y")) { return false; }
+    sum.y +=  iter.get_double();
+    if (!iter.has_next_field() || !iter.find_field_raw("z")) { return false; }
+    sum.z +=  iter.get_double();
+    if (iter.skip_container()) { return false; } // Skip the rest of the tweet object
+    count++;
+  } while (iter.has_next_element());
+
+  return true;
 }
+
+BENCHMARK_TEMPLATE(LargeRandomSum, Iter);
+
+} // unnamed namespace
+} // namespace sum
 } // namespace largerandom
 
 #endif // SIMDJSON_EXCEPTIONS

benchmark/largerandom/ondemand.h
@@ -39,7 +39,45 @@ simdjson_really_inline bool OnDemand::Run(const padded_string &json) {
 
 BENCHMARK_TEMPLATE(LargeRandom, OnDemand);
 
+} // unnamed namespace
+
+namespace sum {
+namespace {
+
+class OnDemand {
+public:
+  simdjson_really_inline bool Run(const padded_string &json);
+
+  simdjson_really_inline my_point &Result() { return sum; }
+  simdjson_really_inline size_t ItemCount() { return count; }
+
+private:
+  ondemand::parser parser{};
+  my_point sum{};
+  size_t count{};
+};
+
+simdjson_really_inline bool OnDemand::Run(const padded_string &json) {
+  sum = {0,0,0};
+  count = 0;
+
+  auto doc = parser.iterate(json);
+  // TODO this sucks, you should be able to just say for ( ... : doc)
+  auto array = doc.get_array();
+  for (ondemand::object coord : array) {
+    sum.x += double(coord["x"]);
+    sum.y += double(coord["y"]);
+    sum.z += double(coord["z"]);
+    count++;
+  }
+
+  return true;
 }
+
+BENCHMARK_TEMPLATE(LargeRandomSum, OnDemand);
+
+} // unnamed namespace
+} // namespace sum
 } // namespace largerandom
 
 #endif // SIMDJSON_EXCEPTIONS